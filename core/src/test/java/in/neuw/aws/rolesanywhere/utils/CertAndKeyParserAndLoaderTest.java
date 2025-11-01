6847cdaa742630be98ef3c2f08d01c3accb6ba17
makerMcl@github.com <makerMcl@github.com>
2025-11-01 14:12:03 +0100

    unit test executes successful for supported use cases as documented in README.md since commit 4c1ec1e

--- core/src/test/java/in/neuw/aws/rolesanywhere/utils/CertAndKeyParserAndLoaderTest.java
+++ core/src/test/java/in/neuw/aws/rolesanywhere/utils/CertAndKeyParserAndLoaderTest.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import in.neuw.aws.rolesanywhere.credentials.IAMRolesAnywhereSessionsCredentialsProvider;
 import in.neuw.aws.rolesanywhere.props.AwsRolesAnywhereProperties;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.iam.model.IamException;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CertAndKeyParserAndLoaderTest {
     private static final String PEM_CERT_HEADER = "-----BEGIN CERTIFICATE-----\n";
@@ -54,19 +55,62 @@ class CertAndKeyParserAndLoaderTest {
             jNaDrhcbigdKNMLntbd4tmdz2ZU96ZK9+HO3Gn2pdtoo8FXhAKZPpsJE7fUP8FTZ
             Xv4rvhuMK/coXiRWuPMiTH4W4HIZ90a2dKXbxpQ=""";
 
+
+    @Test
+    void couldBeBase64() {
+        assertTrue(CertAndKeyParserAndLoader.isEncodedBase64(CA_CERT.replace("\n", "").replace("\r", "")));
+        assertTrue(CertAndKeyParserAndLoader.isEncodedBase64(SIGNED_CERT.replace("\n", "").replace("\r", "")));
+    }
+
     private void performTest_encodedCertificateConfiguration(String configuredCertValue) {
         final AwsRolesAnywhereProperties properties = new AwsRolesAnywhereProperties();
-        properties.setRoleArn("");
-        properties.setProfileArn("");
-        properties.setTrustAnchorArn("");
+        properties.setRoleArn("abc");
+        properties.setProfileArn("abc");
+        properties.setTrustAnchorArn("abc");
         properties.setRegion("us-east-1");
         properties.setEncodedX509Certificate(configuredCertValue);
+        properties.setDurationSeconds(30);
+        properties.setEncodedPrivateKey(Base64.getEncoder().encodeToString(
+                ("-----BEGIN PRIVATE KEY-----\n" +
+                        """
+                                MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC54Onrbi2U+QUs
+                                kPI1Lj8mQ8gM+mJT7f7GkOAWlASVJC+IQJSGPRAcEAcNsobnnzlrX4/VP5kAumHP
+                                XR3K3+OWVdAgL25eraSUZJJy1Tr6bC0t3wtu3zmcFDlMBZ+Gs6cxfcDYCN+tB0A/
+                                EK44YJmeTVe3sT1jHFlO4W1N5m8j9bux4eASkuYJC9F4EaZL4gDlQ/C+ujar2LCX
+                                cnfpcttL9fM4cIJITRA8eWt6b9ffbIrzOsyvEo2qAb3KICBz0L8kLie7TidouBY4
+                                jlkK69zI2QfoKDFV4xpJf2OyTmCshb9rgLCYZwnGOh+NNvMjBTKfLd3zH+7pOAJr
+                                IY4Co3zHAgMBAAECggEARDebIeBJskbB810uxv22B2EQ5lJuC8donY9F7oNcOzAQ
+                                kbzBxZJbezEWpZxDjnYNKK7lpqm0iWPdybCu4mnQqLfNrkI6C11G7X8LkEFZZ9aT
+                                riYRmBsslW+Gp7PYnCMK1UsFn0OCc4oZtgt1JROuzT4tVykhEmKxFBX/e3t6hoNk
+                                TC5h6Xs53yX9dRk+eYObXybwiGb4gukr9yn4PoyNOpUyMKTyTnOieSmDU/hu7qC1
+                                l1nBBmJjE4yyxrwevGaCpe7xFgxEGZEjKUDsf8/v5lnbYEznuAyqsvHMtfIMigof
+                                E+PYBpNJH1JwSWdzmr0Gz4p/74+bLq41Vl5t1k7gwQKBgQDrkUa2X1O64Ic+3FS7
+                                R5M8UTwFjuW+p9ExQLKoYkc6vadikA1kJwT12tMk2Q3riUleevdgCClAE+6Ff/LL
+                                k56wfYcBkOtNW+bRpdUJjHKdBSq8RODW78w2fDB68Tm2LViWDdQ7s3/SX+hkOW+R
+                                ZzyDQVPjromRVpzWJ3QeJU2cSQKBgQDKAE5hAXyGdIPB4Cs7n5jWg+nHhDe+HaQk
+                                A14xnSxEpZyjadrDUz5NFD/WLPGqhsLeN6RykYCuoqMN2V3ZyZ4p77x9QyZqcWhb
+                                UzOxfhX39aT+8DUsc2k9+o8+KUYGoHveq0F3zliYZidU7/y3f2gcGHvZ47Fe/vF4
+                                mBrkxB+wjwKBgQDfwZfEZVoYGWv99rV5Sux9D8COmwR5i9g2C3loXinAjyMuiRxf
+                                NH0OtvN2fcIuJ7KMTwsoqbfdCNG49yb93lOO2z1g7HHFgnUGUJnCPykjIyRLvrU/
+                                FHoKiv5V2UpzVDFgHvSoXMEeaEFK6g+issgU+PmhPEmpxvakqMcwV0iYGQKBgQDD
+                                wOQz0ZLxYXxFp4197D3atF8bACBc1rC4TaM8hUnfBHb1TaG4Y7UeUR9D2K4hSRMS
+                                8e0lMaf4lJda1mjCo0Xo8fBBm49g26H7zndr6/oyHxTUEcgV2YoFjAjAfpLj3SXt
+                                NRzuk0L7/RLkluTallc30upCzwqWOo2bBhwZ0Y28NQKBgQCzwPPdYRRtTpiR96i+
+                                JkQz+89VZjDPuqw8c5/eGRxrgOxrqjucwEFJJRQHLAHRKgGRVbgLHZ8j1qYG53iE
+                                rkaDQNdKis0SpvzaYkkFEcFJcuUBQyD9LTYpF1z/9CBGsUp3yc8BfFh1sO458OSS
+                                yyZH6Iw71i2/SE82k9+hW4ndJA==
+                                """.replace("\n", "").replace("\r", "") +
+                        "\n-----END PRIVATE KEY-----"
+                ).getBytes(StandardCharsets.US_ASCII)));
         try (final var prov = new IAMRolesAnywhereSessionsCredentialsProvider
                 .Builder(properties, new ObjectMapper())
                 .prefetch(true)
                 .asyncCredentialUpdateEnabled(true)
                 .build()) {
             assertNotNull(prov);
+        } catch (IamException e) {
+            // success case, configuration could be parsed and request to AWS service was issued
+            assertEquals("Error while trying to connect to AWS ROLES ANYWHERE", e.getMessage());
         }
     }
 
